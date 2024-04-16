4e56024c7ec6e8b0c4991801582ba42c0f09c1e0
PSP369 <156859865+PSP369@users.noreply.github.com>
2024-04-16 19:09:42 -0400

    changes in bublesort
    added a button

--- examples/chapter13/01-BubbleSort.js
+++ examples/chapter13/01-BubbleSort.js
@@ -1,12 +1,31 @@
 const { bubbleSort } = PacktDataStructuresAlgorithms;
 
-function createNonSortedArray(){
+function RunBubbleSort(){
+  var valuepicked = document.getElementById('numberOfValues').value;
+  if (valuepicked > 0) {
+      const array = bubbleSort(createNonSortedArray(valuepicked));
+      console.log(array);
+  } else {
+    console.error("");
+    alert("invalid input");
+  }
+
+}
+// function createNonSortedArray(){
+//   var array = [];
+//   for (let i = 55; i > 0; i--){
+//       array.push(i);
+//   }
+//   return array;
+// }
+
+function createNonSortedArray(Parameter){
   var array = [];
-  for (let i = 5; i > 0; i--){
+  for (let i = Parameter; i > 0; i--){
       array.push(i);
   }
   return array;
 }
 
-const array = bubbleSort(createNonSortedArray());
-console.log(array);
+// const array = bubbleSort(createNonSortedArray());
+// console.log(array);
